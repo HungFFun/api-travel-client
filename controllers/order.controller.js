075ca3ea7e7cf52361bd90154b77c3b65a93e2b5
HungFun <hung.fun40@gmail.com>
2021-05-10 20:57:49 +0700

    infor booking tour

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -13,74 +13,119 @@ const getAllOrder = async (req, res) => {
     if (listOrder !== null) {
       res.status(200).send(listOrder);
     }
-    res.status(200).send({message:ORDERCONSTANT.NOT_FIND_ORDER});
+    res.status(200).send({ message: ORDERCONSTANT.NOT_FIND_ORDER });
   } catch (error) {
-    res.status(500).send({message:ORDERCONSTANT.SYSTEM_ERROR});
+    res.status(500).send({ message: ORDERCONSTANT.SYSTEM_ERROR });
   }
 };
 
 const createOrderForCustomer = async (req, res) => {
   try {
-    var customerOrder = req.body.customer;
-    var getSeatDetail = req.body.seatDetail;
-    var getOrder = req.body.order;
-    const customerExist = await customer.findOne({
-      phone: customerOrder.phone,
-      email: customerOrder.email,
-    });
-    if (customerExist !== null) {
-      getSeatDetail.customerId = customerExist._id;
-      var newSeatDetail = new seatDetail({
-        tourId: getSeatDetail.tourId,
-        listCutomerTour: getSeatDetail.listCutomerTour,
-        customerId: getSeatDetail.customerId,
-        amountRoom: getSeatDetail.amountRoom,
-        totalPrice: getSeatDetail.totalPrice,
-      });
-      newSeatDetail.save(function (err) {
-        if (err) res.status(500).send({message:ORDERCONSTANT.CREATE_SEAT_FAIL});
-      });
-      var newOrder = new order({
-        orderCode: getOrder.orderCode,
-        orderDate: getOrder.orderDate,
-        listOrderDetail: null,
-        seatDetail: newSeatDetail._id,
-        total: getOrder.total,
-      });
-      newOrder.save(function (err) {
-        if (err) res.status(500).send({message:ORDERCONSTANT.CREATE_ORDER_FAIL});
-      });
-    } else {
-      var newCustomer = new customer(customerOrder);
-      newCustomer.save(function (err) {
-        if (err)
-          res.status(500).send({message:CUSTOMERCONSTANT.ADD_CUSTOMER_FAIL});
-      });
-      getSeatDetail.customerId = newCustomer._id;
-      var newSeatDetail = new seatDetail({
-        tourId: getSeatDetail.tourId,
-        listCutomerTour: getSeatDetail.listCutomerTour,
-        customerId: getSeatDetail.customerId,
-        amountRoom: getSeatDetail.amountRoom,
-        totalPrice: getSeatDetail.totalPrice,
-      });
-      newSeatDetail.save(function (err) {
-        if (err) res.status(500).send({message:ORDERCONSTANT.CREATE_SEAT_FAIL});
-      });
-      var newOrder = new order({
-        orderCode: getOrder.orderCode,
-        orderDate: getOrder.orderDate,
-        listOrderDetail: null,
-        seatDetail: newSeatDetail._id,
-        total: getOrder.total,
-      });
-      newOrder.save(function (err) {
-        if (err) res.status(500).send({message:ORDERCONSTANT.CREATE_ORDER_FAIL});
-      });
-    }
-    res.status(200).send(newOrder);
+    const {
+      tour,
+      customer,
+      productCart,
+      inforBooking,
+      inforAdults,
+      inforChildren,
+      inforYoung,
+    } = req.body;
+
+    console.log('------ ------tour được chon -----------');
+    console.log(tour);
+    console.log('-------------- thông tin liên lạc người đăng ký-----------');
+    console.log(customer);
+    console.log('-------------- sản phẩm được chọn mua-----------');
+    console.log(productCart);
+    console.log(
+      '-------------------thông tin booking----------------------------------------'
+    );
+    console.log(inforBooking);
+    console.log(
+      '--------------------------danh sách khách hàng người lơn---------------------------------'
+    );
+    console.log(inforAdults);
+    console.log(
+      '-----------------danh sách khách hàng trẻ em------------------------------------------'
+    );
+    console.log(inforChildren);
+    console.log(
+      '----------------danh sách khách hàng em bé-------------------------------------------'
+    );
+    console.log(inforYoung);
+
+    // var customerOrder = req.body.customer;
+    // var getSeatDetail = req.body.seatDetail;
+    // var getOrder = req.body.order;
+
+    // // tìm khách hang đã tồn tại hay chưa
+    // const customerExist = await customer.findOne({
+    //   phone: customerOrder.phone,
+    //   email: customerOrder.email,
+    // });
+
+    // // nếu khách hàng tông tại thực hiện lưu thông tin đăng ký tour
+    // if (customerExist !== null) {
+    //   getSeatDetail.customerId = customerExist._id;
+
+    //   const newSeatDetail = new seatDetail({
+    //     tourId: getSeatDetail.tourId,
+    //     listCutomerTour: getSeatDetail.listCutomerTour,
+    //     customerId: getSeatDetail.customerId,
+    //     amountRoom: getSeatDetail.amountRoom,
+    //     totalPrice: getSeatDetail.totalPrice,
+    //   });
+
+    //   newSeatDetail.save(function (err) {
+    //     if (err)
+    //       res.status(500).send({ message: ORDERCONSTANT.CREATE_SEAT_FAIL });
+    //   });
+    //   const newOrder = new order({
+    //     orderCode: getOrder.orderCode,
+    //     orderDate: getOrder.orderDate,
+    //     listOrderDetail: null,
+    //     seatDetail: newSeatDetail._id,
+    //     total: getOrder.total,
+    //   });
+    //   newOrder.save(function (err) {
+    //     if (err)
+    //       res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
+    //   });
+    // }
+    // // nếu khách hàng không tồn tại thực hiện đăng ký thông tin khách hàng mới
+    // else {
+    //   const newCustomer = new customer(customerOrder);
+    //   newCustomer.save(function (err) {
+    //     if (err)
+    //       res.status(500).send({ message: CUSTOMERCONSTANT.ADD_CUSTOMER_FAIL });
+    //   });
+    //   getSeatDetail.customerId = newCustomer._id;
+    //   const newSeatDetail = new seatDetail({
+    //     tourId: getSeatDetail.tourId,
+    //     listCutomerTour: getSeatDetail.listCutomerTour,
+    //     customerId: getSeatDetail.customerId,
+    //     amountRoom: getSeatDetail.amountRoom,
+    //     totalPrice: getSeatDetail.totalPrice,
+    //   });
+    //   newSeatDetail.save(function (err) {
+    //     if (err)
+    //       res.status(500).send({ message: ORDERCONSTANT.CREATE_SEAT_FAIL });
+    //   });
+    //   const newOrder = new order({
+    //     orderCode: getOrder.orderCode,
+    //     orderDate: getOrder.orderDate,
+    //     listOrderDetail: null,
+    //     seatDetail: newSeatDetail._id,
+    //     total: getOrder.total,
+    //   });
+    //   newOrder.save(function (err) {
+    //     if (err)
+    //       res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
+    //   });
+    // }
+    // res.status(200).send(newOrder);
   } catch (error) {
-    if (err) res.status(500).send({message:ORDERCONSTANT.SYSTEM_ERROR});
+    if (err) res.status(500).send({ message: ORDERCONSTANT.SYSTEM_ERROR });
   }
 };
 module.exports = {
--- models/seatDetail.model.js
+++ models/seatDetail.model.js
@@ -1,35 +1,40 @@
 var mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate-v2')
+const mongoosePaginate = require('mongoose-paginate-v2');
 
-var SeatDetailSchema = mongoose.Schema({
-    tourId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "tour",
+var SeatDetailSchema = mongoose.Schema(
+  {
+    tourId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'tour',
     },
-    listCutomerTour:[{
-        nameCustomer:{
-            type:String
+    listCutomerTour: [
+      {
+        nameCustomer: {
+          type: String,
         },
-        birthDay:{
-            type:Date
+        birthDay: {
+          type: Date,
         },
-        gender:{
-            type:String
+        gender: {
+          type: String,
         },
-        price:{
-            type:Number
-        }
-    }],
-    customerId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "customer",
+        typeOfTicket: {
+          type: String,
+        },
+      },
+    ],
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'customer',
     },
-    amountRoom:{
-        type:Number
+    amountRoom: {
+      type: Number,
     },
-    totalPrice:{
-        type:Number
-    }
-},{ versionKey: false });
+    totalPrice: {
+      type: Number,
+    },
+  },
+  { versionKey: false }
+);
 SeatDetailSchema.plugin(mongoosePaginate);
-module.exports = mongoose.model('seatDetail',SeatDetailSchema,'seatDetails');
+module.exports = mongoose.model('seatDetail', SeatDetailSchema, 'seatDetails');
