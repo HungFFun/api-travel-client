1a0c4f67515b2bdca391e5eeebf0301d0fbb081f
HungFun <hung.fun40@gmail.com>
2021-05-11 11:31:48 +0700

    fix res send

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,10 +1,10 @@
-var order = require('../models/order.model');
-var customerModel = require('../models/customer.model');
-var orderDetailModel = require('../models/orderDetail.model')
-var seatDetail = require('../models/seatDetail.model');
-var ORDERCONSTANT = require('../constants/order.constant');
-var CUSTOMERCONSTANT = require('../constants/customer.constant');
-var moment = require('moment');
+const order = require('../models/order.model');
+const customerModel = require('../models/customer.model');
+const orderDetailModel = require('../models/orderDetail.model');
+const seatDetail = require('../models/seatDetail.model');
+const ORDERCONSTANT = require('../constants/order.constant');
+const CUSTOMERCONSTANT = require('../constants/customer.constant');
+const moment = require('moment');
 
 const getAllOrder = async (req, res) => {
   try {
@@ -34,19 +34,22 @@ const createOrderForCustomer = async (req, res) => {
 
     // Tạo danh sách sản phẩm cần mua
     var listOrderDetail = [];
-    for (let index = 0; index < productCart.length; index++) {
-      var newOrderDetail = new orderDetailModel({
-        quantily: productCart[index].quantily,
-        price: productCart[index].price,
-        productId: productCart[index]._id,
-        totalPrice: productCart[index].quantily * productCart[index].price
-      })
-      console.log(newOrderDetail);
-      listOrderDetail.push(newOrderDetail._id)
-      newOrderDetail.save(function (error) {
-        if (error)
-          res.status(500).send({ message: "Tạo chi tiết các sản phẩm thất bại" });
+    for (let i = 0; i < productCart.length; i++) {
+      const newOrderDetail = new orderDetailModel({
+        quantity: productCart[i].quantity,
+        price: productCart[i].price,
+        productId: productCart[i]._id,
+        totalPrice: productCart[i].quantity * productCart[i].price,
       });
+      listOrderDetail.push(newOrderDetail._id);
+      newOrderDetail
+        .save()
+        .then((value) => {})
+        .catch((error) => {
+          res
+            .status(500)
+            .send({ message: 'Tạo chi tiết các sản phẩm thất bại' });
+        });
     }
 
     // Tạo danh sách người đi
@@ -54,43 +57,49 @@ const createOrderForCustomer = async (req, res) => {
     var priceCustomerTour = 0;
     if (inforAdults !== undefined) {
       for (let index = 0; index < inforAdults.length; index++) {
-        var customerTour = {
+        const customerTour = {
           nameCustomer: inforAdults[index].name,
           birthDay: inforAdults[index].birthday,
           gender: inforAdults[index].gender,
           typeOfTicket: inforAdults[index].typeOfTicket,
-        }
+        };
         listCustomerTour.push(customerTour);
       }
-      priceCustomerTour = priceCustomerTour + inforAdults.length * tour.priceDetail.adult;
+      priceCustomerTour =
+        priceCustomerTour + inforAdults.length * tour.priceDetail.adult;
     }
     if (inforChildren !== undefined) {
       for (let index = 0; index < inforChildren.length; index++) {
-        var customerTour = {
+        const customerTour = {
           nameCustomer: inforChildren[index].name,
           birthDay: inforChildren[index].birthday,
           gender: inforChildren[index].gender,
           typeOfTicket: inforChildren[index].typeOfTicket,
-        }
+        };
         listCustomerTour.push(customerTour);
       }
-      priceCustomerTour = priceCustomerTour + inforChildren.length * tour.priceDetail.underTheAgeOfTwelve;
+      priceCustomerTour =
+        priceCustomerTour +
+        inforChildren.length * tour.priceDetail.underTheAgeOfTwelve;
     }
     if (inforYoung !== undefined) {
       for (let index = 0; index < inforYoung.length; index++) {
-        var customerTour = {
+        const customerTour = {
           nameCustomer: inforYoung[index].name,
           birthDay: inforYoung[index].birthday,
           gender: inforYoung[index].gender,
           typeOfTicket: inforYoung[index].typeOfTicket,
-        }
+        };
         listCustomerTour.push(customerTour);
       }
-      priceCustomerTour = priceCustomerTour + inforYoung.length * tour.priceDetail.underTheAgeOfFive;
-
+      priceCustomerTour =
+        priceCustomerTour +
+        inforYoung.length * tour.priceDetail.underTheAgeOfFive;
     }
     // tìm khách hang đã tồn tại hay chưa
-    const customerExist = await customerModel.findOne({ $or: [{ phone: customer.phone }, { email: customer.email }] });
+    const customerExist = await customerModel.findOne({
+      $or: [{ phone: customer.phone }, { email: customer.email }],
+    });
 
     // nếu khách hàng tông tại thực hiện lưu thông tin đăng ký tour
     if (customerExist !== null) {
@@ -101,11 +110,14 @@ const createOrderForCustomer = async (req, res) => {
         // amountRoom: getSeatDetail.amountRoom,
         totalPrice: priceCustomerTour,
       });
-      console.log("seat"  + newSeatDetail);
-      newSeatDetail.save(function (err) {
-        if (err)
+      console.log('seat' + newSeatDetail);
+      newSeatDetail
+        .save()
+        .then((value) => {})
+        .catch((error) => {
           res.status(500).send({ message: ORDERCONSTANT.CREATE_SEAT_FAIL });
-      });
+        });
+
       const newOrder = new order({
         orderCode: inforBooking.bookId,
         orderDate: inforBooking.dateBook,
@@ -113,20 +125,19 @@ const createOrderForCustomer = async (req, res) => {
         seatDetail: newSeatDetail._id,
         total: inforBooking.totalMoney,
       });
-      console.log(newOrder);
-      newOrder.save(function (err) {
-        if (err) {
-          res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
-        }
-        else {
+      newOrder
+        .save()
+        .then((value) => {
           res.status(200).send(newOrder);
-        }
-      });
+        })
+        .catch((error) => {
+          res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
+        });
     }
     // nếu khách hàng không tồn tại thực hiện đăng ký thông tin khách hàng mới
     else {
       const newCustomer = new customerModel(customer);
-      console.log(newCustomer);
+      // console.log(newCustomer);
       newCustomer.save(function (err) {
         if (err)
           res.status(500).send({ message: CUSTOMERCONSTANT.ADD_CUSTOMER_FAIL });
@@ -138,7 +149,7 @@ const createOrderForCustomer = async (req, res) => {
         // amountRoom: getSeatDetail.amountRoom,
         totalPrice: priceCustomerTour,
       });
-      console.log(newSeatDetail);
+      // console.log(newSeatDetail);
       newSeatDetail.save(function (err) {
         if (err)
           res.status(500).send({ message: ORDERCONSTANT.CREATE_SEAT_FAIL });
@@ -150,20 +161,18 @@ const createOrderForCustomer = async (req, res) => {
         seatDetail: newSeatDetail._id,
         total: inforBooking.totalMoney,
       });
-      console.log(newOrder);
-      newOrder.save(function (err) {
-        if (err) {
-          res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
-        }
-        else {
+      // console.log(newOrder);
+      newOrder
+        .save()
+        .then((value) => {
           res.status(200).send(newOrder);
-        }
-      });
+        })
+        .catch((error) => {
+          res.status(500).send({ message: ORDERCONSTANT.CREATE_ORDER_FAIL });
+        });
     }
-
   } catch (error) {
-    if (error)
-      res.status(500).send({ message: ORDERCONSTANT.SYSTEM_ERROR });
+    if (error) res.status(500).send({ message: ORDERCONSTANT.SYSTEM_ERROR });
   }
 };
 module.exports = {
